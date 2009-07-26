27600c4a1b4bbbb7452ec26b2dea5b4c1a5974f6
Phize <info@phize.net>
2009-07-26 01:30:24 +0000

    git-svn-id: svn://localhost/MODx/assets/plugins/phieditedon/trunk@53 6ce02de6-cfb7-2e46-b63e-5161d7ce3690

---
+++ assets/plugins/phieditedon/phieditedon.inc.php
@@ -0,0 +1,43 @@
+<?php
+/*==============================================================================
+  Title: phiEditedon
+  Category: Plugin
+  Author: Phize
+  Author URI: http://phize.net
+  License: GNU General Public License(http://www.gnu.org/licenses/gpl.html)
+  Version: 1.0.0
+  Last Update: 2007-08-28T20:34:12+09:00
+==============================================================================*/
+
+
+
+
+
+function modx_plugin_phiEditedon_getTemplateVar($modx, $tvName, $id, $published, $deleted) {
+    $result= array();
+
+    $docRow = $modx->getDocument($id, '*', $published, $deleted);
+
+    if (!$docRow) {
+        return false;
+    }
+
+    if ($docgrp = $modx->getUserDocGroups()) {
+        $docgrp = implode(",", $docgrp);
+    }
+
+    $sql = "SELECT tv.id, IF (tvc.value != '', tvc.value, tv.default_text) as value ";
+    $sql .= "FROM " . $modx->getFullTableName('site_tmplvars')." tv ";
+    $sql .= "INNER JOIN " . $modx->getFullTableName('site_tmplvar_templates')." tvtpl ON tvtpl.tmplvarid = tv.id ";
+    $sql .= "LEFT JOIN " . $modx->getFullTableName('site_tmplvar_contentvalues')." tvc ON tvc.tmplvarid=tv.id AND tvc.contentid = '" . $id . "' ";
+    $sql .= "WHERE tv.name IN ('" . $tvName . "') AND tvtpl.templateid = " . $docRow['template'];
+
+    $rs = $modx->dbQuery($sql);
+
+    for ($i = 0; $i < @ $modx->recordCount($rs); $i ++) {
+        array_push($result, @ $modx->fetchRow($rs));
+    }
+
+    return ($result != false) ? $result[0] : false;
+}
+?>
